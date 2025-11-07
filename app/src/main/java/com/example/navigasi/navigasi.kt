b1eaf9b985615d17c6060a4c24d2ef9d36a70804
Cla1reee <h.harimurti.ft23@mail.umy.ac.id>
2025-11-07 15:29:22 +0700

    menambahkan codingan navigasi.kt

--- app/src/main/java/com/example/navigasi/navigasi.kt
+++ app/src/main/java/com/example/navigasi/navigasi.kt
@@ -1,1 +1,51 @@
 package com.example.navigasi
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.navigasi.view.FormIsian
+import com.example.navigasi.view.TampilanData
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+enum class Navigasi {
+    Formulir,
+    Detail
+}
+
+@Composable
+fun DataApp(
+    navController: NavHostController = rememberNavController(),
+    modifier: Modifier = Modifier
+){
+    Scaffold { isiRuang ->
+        NavHost(
+            navController = navController,
+            startDestination = Navigasi.Formulir.name,
+
+            modifier= Modifier.padding( paddingValues = isiRuang)
+        ) {
+            composable(route = Navigasi.Formulir.name) {
+                FormIsian(
+                    onSubmitBtnClick = {
+                        navController.navigate(route = Navigasi.Detail.name)
+                    }
+                )
+            }
+            composable(route = Navigasi.Detail.name) {
+                TampilanData(
+                    onBackBtnClick = { cancelAndBackToFormulir(navController) }
+                )
+            }
+        }
+    }
+}
+
+private fun cancelAndBackToFormulir(
+    navController: NavHostController
+){
+    navController.popBackStack(route = Navigasi.Formulir.name, inclusive = false)
+}
